93be10778ff6b8e1bac393516244ec4e8368eb81
Antoine Ridard <papatou15@hotmail.com>
2024-11-09 08:37:08 -0500

    added responsive to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,8 +7,8 @@ interface Props {
 
 const Footer: React.FC<Props> = ({tabs, contacts}) => {
     return(
-        <div className="flex flex-row items-center px-52 bg-primary h-[400px] font-text text-lg text-off-white border-[5px] border-black">
-            <div className="w-[33%] h-[70%] text-xl flex flex-col justify-center items-center">
+        <div className="flex flex-col lg:flex-row items-center px-20 lg:px-52 bg-primary lg:h-[400px] font-text text-lg text-off-white border-[5px] border-black">
+            <div className="lg:w-[33%] lg:h-[70%] text-xl flex flex-col justify-center items-center">
                 <h2 className="p-3 font-semiblod text-3xl">Navigation</h2>
                 {tabs ? tabs.map((tab) => 
                     <div key={tab._id}>
@@ -16,15 +16,15 @@ const Footer: React.FC<Props> = ({tabs, contacts}) => {
                     </div>
                 ) : "no tabs"}
             </div>
-            <div className="w-full h-[70%] "></div>
-            <div className="w-[33%] h-[70%]  flex flex-col justify-center items-center">
-                <div className="flex flex-col m-3 items-center">
+            <div className="w-full lg:h-[70%] "></div>
+            <div className="lg:w-[33%] lg:h-[70%] flex flex-col justify-center items-center">
+                <div className="flex flex-col p-3 items-center">
                     <h3 className="p-3 font-semibold">Pour nous joindre</h3>
                     <p>Téléphone: {contacts.telephone}</p>
                     <p>{contacts.email}</p>
                     <p>{contacts.adress}</p>
                 </div>
-                <div className="flex flex-col m-3 items-center">
+                <div className="flex flex-col p-3 items-center">
                     <h3 className="p-3 font-semibold">Liens utiles:</h3>
                     <p>Prévention suicide: 988</p>
                     <p>Urgence: 911</p>

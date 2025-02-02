2428c008c29949c01d371286da8b01cdcef9868f
Antoine Ridard <papatou15@hotmail.com>
2025-02-02 16:08:26 -0500

    typing to content block

--- app/components/ContentBlock.tsx
+++ app/components/ContentBlock.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 
 export default function ContentBlock({ children }: { children: ReactNode }) {
     return (
-        <div className="flex flex-col lg:mx-24 lg:my-12">
+        <div className="flex flex-col mx-12 my-7lg:mx-24 lg:my-12">
             {children}
         </div>
     )

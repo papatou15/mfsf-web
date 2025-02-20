dc592c98587322571cac5f3b6223ecf199aaa4cc
Antoine Ridard <papatou15@hotmail.com>
2025-02-19 21:40:16 -0500

    banner styles

--- app/components/Banners.tsx
+++ app/components/Banners.tsx
@@ -9,24 +9,38 @@ interface BannerProps {
 
 const Banner = ({ banner = [] }: BannerProps) => {
 
-    const shownBanner = banner[0] ?? null
+    const shownBanner = banner[0] ?? null;
 
     return (
         <>
-            {shownBanner.isActive &&
+            {shownBanner.isActive && (
                 <div
                     style={{ backgroundColor: shownBanner.bgColor?.hex }}
-                    className={`w-full h-20 border-b-2 border-black flex items-center overflow-hidden`}
+                    className={`w-full h-20 border-b-2 border-black flex items-center overflow-hidden relative`}
                 >
-                    <MFLink className="w-2/5 h-full object-contain " _type={"button"} link={shownBanner.link ?? ""}>
-                        <div className="w-full h-full ">
-                            <img src={sanityImgUrl(shownBanner.bannerBgImage).url()} alt="banner image" className="w-full h-full object-contain" />
+                    {shownBanner.bannerBgImage ? (
+                        <div className="absolute inset-0">
+                            <img src={sanityImgUrl(shownBanner.bannerBgImage).url()} alt="banner image" className="w-full h-full object-cover" />
+                            <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent opacity-50"></div>
                         </div>
-                    </MFLink>
+                    ) : (
+                        <div className="absolute inset-0 bg-gradient-to-r from-black to-transparent opacity-50"></div>
+                    )}
+                    {shownBanner.link ? (
+                        <MFLink className="w-full h-full z-10" _type={"button"} link={shownBanner.link}>
+                            <div className="w-full h-full flex items-center justify-center">
+                                <span className="text-white">{shownBanner.textContent}</span>
+                            </div>
+                        </MFLink>
+                    ) : (
+                        <div className="w-full h-full flex items-center justify-center z-10">
+                            <span className="text-white">{shownBanner.textContent}</span>
+                        </div>
+                    )}
                 </div>
-            }
+            )}
         </>
-    )
+    );
 }
 
-export default Banner
+export default Banner;

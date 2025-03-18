508de90f4fda4944a1bdfd5a85c68703c1916ef0
Antoine Ridard <papatou15@hotmail.com>
2025-03-17 23:00:04 -0400

    button restyling

--- app/components/MFButton.tsx
+++ app/components/MFButton.tsx
@@ -11,8 +11,8 @@ export const buttonStyle = tv({
     base: 'w-auto flex justify-center items-center font-text',
     variants: {
         styling: {
-            coloredbg: 'h-14 md:h-16 bg-yellow-1 hover:bg-yellow-2 m-5 p-8 rounded-2xl text-black border-2 border-black text-2xl hover:shadow-button transition-all duration-100 hover:translate-x-1 hover:-translate-y-1 active:shadow-none active:translate-x-0 active:translate-y-0 active:bg-accent-1',
-            smallbg: 'h-6 bg-yellow-1 hover:bg-yellow-2 m-5 p-4 md:p-6 rounded-2xl text-black border-2 border-black text-lg hover:shadow-button transition-all duration-100 hover:translate-x-1 hover:-transltate-y-1 active:shadow-none active:translate-x-0 active:translate-y-0 active:bg-accent-1',
+            coloredbg: 'h-14 md:h-16 bg-custom-beige hover:bg-off-white m-5 p-8 rounded-2xl text-black border-2 border-black text-2xl hover:shadow-button transition-all duration-100 hover:translate-x-1 hover:-translate-y-1 active:shadow-none active:translate-x-0 active:translate-y-0 active:bg-slate-500',
+            smallbg: 'h-6 bg-custom-beige hover:bg-off-white m-5 p-4 md:p-6 rounded-2xl text-black border-2 border-black text-lg hover:shadow-button transition-all duration-100 hover:translate-x-1 hover:-transltate-y-1 active:shadow-none active:translate-x-0 active:translate-y-0 active:bg-slate-500',
             colorless: 'h-14 md:h-16 bg-transparent m-5 p-8',
             smallcolorless: '',
         }

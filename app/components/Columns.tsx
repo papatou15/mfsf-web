b1c55c1ca0a7dfd5ca3fe582946d9e7e49bfbc9b
Antoine Ridard <papatou15@hotmail.com>
2025-03-18 16:39:39 -0400

    column styling

--- app/components/Columns.tsx
+++ app/components/Columns.tsx
@@ -6,9 +6,9 @@ const Columns: React.FC<ColumnBlock> = ({ layout, column1, column2, column3, _ty
     const columns = layout === 'three' ? [column1, column2, column3] : [column1, column2];
 
     return (
-        <div className={`${_type} ${layout === 'three' ? 'three-columns' : 'two-columns'} flex flex-col items-start lg:flex-row w-full `}>
+        <div className={`${_type} ${layout === 'three' ? 'three-columns' : 'two-columns'} flex flex-col items-start xl:flex-row w-full `}>
             {columns.map((column, columnIndex) => (
-                <div key={columnIndex} className="column w-full flex flex-row">
+                <div key={columnIndex} className="column w-full flex flex-row justify-center">
                     {column?.map((item, itemIndex) => (
                         <SectionRenderer key={item._key || itemIndex} section={item} />
                     ))}

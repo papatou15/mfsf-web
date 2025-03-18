280ddc0a2790222e06cab655e512f4a0f4bfdf9a
Antoine Ridard <papatou15@hotmail.com>
2025-03-18 16:30:56 -0400

    carousel revamp

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,60 +1,91 @@
 /* eslint-disable @next/next/no-img-element */
-"use client"
+"use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import type { Swiper as SwiperClass } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { Carousel as CarouselType } from "@/sanity.types";
 import sanityImgUrl from "../sanityImageBuilder";
 import MFLink from "./MFLink";
 import Typography from "./Typography/Typography";
 import typographyTheme from "./theme/Typography";
+import MFButton from "./MFButton";
 
 interface CarouselProps extends CarouselType {
-    images: CarouselType['images'];
-    title: CarouselType['title'];
+    images: CarouselType["images"];
+    title: CarouselType["title"];
 }
 
 const Carousel = ({ images, title, _type }: CarouselProps) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const maxImagesToShow = 7;
-
-    const next = () => {
-        setCurrentIndex((prevIndex) => images ? (prevIndex + 1) % images.length : 0);
-    };
-
-    const prev = () => {
-        setCurrentIndex((prevIndex) => images ? (prevIndex - 1 + images.length) % images.length : 0);
-    };
-
-    useEffect(() => {
-        const interval = setInterval(next, 10000); // Cycle every 10 seconds
-        return () => clearInterval(interval); // Cleanup interval on component unmount
-    }, );
-
-    const displayedImages = [];
-    for (let i = 0; i < maxImagesToShow; i++) {
-        displayedImages.push(images?.[(currentIndex + i) % images.length]);
-    }
+    const [swiperInstance, setSwiperInstance] = useState<SwiperClass | null>(null);
 
     return (
-        <div className={`${_type} w-full px-24`}>
-            {title && <Typography as="h2" className={`${typographyTheme({ size: 'h4' })} text-center my-14`}>{title}</Typography>}
-            <div className="carousel flex items-center space-x-4 overflow-hidden">
-                <button onClick={prev} className="p-4 bg-white rounded-xl border-black border-2 hover:bg-yellow-1 transition-all duration-200 ease-in-out">
+        <div className={`${_type} w-full mx-24 relative`}>
+            {title && (
+                <Typography
+                    as="h2"
+                    className={`${typographyTheme({ size: "h4" })} text-center my-14`}
+                >
+                    {title}
+                </Typography>
+            )}
+
+            <div className="relative flex items-center">
+                {/* Navigation Buttons */}
+                <MFButton
+                    _type="button"
+                    style="smallbg"
+                    onClick={() => swiperInstance?.slidePrev()} // Swiper instance method
+                    className="absolute left-0 z-10"
+                >
                     <FaArrowLeft />
-                </button>
-                <div className="carousel-images-wrapper overflow-hidden relative w-full">
-                    <div className="carousel-images flex transition-transform duration-500 ease-in-out">
-                        {displayedImages.map((image, index) => (
-                            <MFLink link={image?.link ?? '#'} key={image?._key} style={"smallcolorless"} extraCSS="!h-40 !w-40 mx-2 overflow-hidden bg-white border-black rounded-2xl border-2" _type={"button"}>
-                                <img key={index} src={sanityImgUrl(image?.carouselImage).url()} alt={`Slide ${index}`} className="object-contain" />
+                </MFButton>
+
+                {/* Swiper Carousel */}
+                <Swiper
+                    modules={[Navigation, Autoplay]}
+                    spaceBetween={20}
+                    slidesPerView={3} // Default
+                    autoplay={{ delay: 10000, disableOnInteraction: false }}
+                    breakpoints={{
+                        860: { slidesPerView: 3 }, // Small screens
+                        1300: { slidesPerView: 5 }, // Medium screens
+                        1600: { slidesPerView: 7 }, // Large screens
+                    }}
+                    onSwiper={setSwiperInstance} // Store Swiper instance
+                    className="carousel"
+                >
+                    {images?.map((image) => (
+                        <SwiperSlide key={image?._key}>
+                            <MFLink
+                                link={image?.link ?? "#"}
+                                style="smallcolorless"
+                                extraCSS="h-40 w-40 mx-2 overflow-hidden bg-white rounded-2xl flex items-center justify-center"
+                                _type="button"
+                            >
+                                <img
+                                    src={sanityImgUrl(image?.carouselImage).url()}
+                                    alt="Carousel Image"
+                                    className="object-fill w-full h-full rounded-2xl"
+                                />
                             </MFLink>
-                        ))}
-                    </div>
-                </div>
-                <button onClick={next} className="p-4 bg-white rounded-xl border-black border-2 hover:bg-yellow-1 transition-all duration-200 ease-in-out">
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+
+                <MFButton
+                    _type="button"
+                    style="smallbg"
+                    onClick={() => swiperInstance?.slideNext()} // Swiper instance method
+                    className="absolute right-0 z-10"
+                >
                     <FaArrowRight />
-                </button>
+                </MFButton>
             </div>
         </div>
     );

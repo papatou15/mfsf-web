116528c7b3e694d3bc7625fb1931852387d544c0
Antoine Ridard <papatou15@hotmail.com>
2025-03-18 22:21:00 -0400

    typography sizes responsive

--- app/components/theme/Typography/index.tsx
+++ app/components/theme/Typography/index.tsx
@@ -4,18 +4,19 @@ const typographyTheme = tv({
     base: 'text-shadow-inherit',
     variants: {
         size: {
-            h1: 'text-7xl',
-            h2: 'text-5xl',
-            h3: 'text-4xl',
-            h4: 'text-3xl',
-            h5: 'text-2xl',
-            h6: 'text-xl',
-            leading: 'text-base font-bold leading-normal',
-            paragraph: 'text-xl inline',
-            footnote: 'text-base',
+            h1: 'text-5xl sm:text-6xl md:text-7xl',
+            h2: 'text-4xl sm:text-5xl md:text-6xl',
+            h3: 'text-3xl sm:text-4xl md:text-5xl',
+            h4: 'text-2xl sm:text-3xl md:text-4xl',
+            h5: 'text-xl sm:text-2xl md:text-3xl',
+            h6: 'text-lg sm:text-xl md:text-2xl',
+            leading: 'text-sm sm:text-base font-bold leading-normal',
+            paragraph: 'text-lg sm:text-xl inline',
+            footnote: 'text-sm sm:text-base',
             xs: 'text-xs'
         }
     }
 })
+
 
 export default typographyTheme

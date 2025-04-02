8240af6bbb85840798f805e3a14872e07e2511c7
Antoine Ridard <papatou15@hotmail.com>
2025-04-02 16:33:23 -0400

    made hero responsive

--- app/components/MFHero.tsx
+++ app/components/MFHero.tsx
@@ -10,8 +10,8 @@ const MFHero: React.FC<MFHeroProps> = ({ title, subTitle, image, layout, _type, 
 
     return (
         <div className={`${_type} py-20 flex justify-center items-center`}>
-            <div style={{ backgroundColor: bgColor?.hex ? bgColor?.hex : ''}} className={`py-12 px-32 rounded-[1.5rem] shadow-big-box-bg flex ${layout == 'default' ? 'md:flex-row' : 'md:flex-row-reverse'} justify-center items-center`}>
-                <div className="w-[50%] flex flex-col justify-center items-start">
+            <div style={{ backgroundColor: bgColor?.hex ? bgColor?.hex : ''}} className={`py-12 md:px-32 sm:rounded-xl shadow-big-box-bg flex flex-col ${layout == 'default' ? 'md:flex-row' : 'md:flex-row-reverse'} justify-center items-center`}>
+                <div className="md:w-[50%] flex flex-col justify-center items-start">
                     <Typography as="h1" className={`${typographyTheme({ size: 'h1' })} !shadow-text py-4`}>
                         {title}
                     </Typography>
@@ -19,8 +19,8 @@ const MFHero: React.FC<MFHeroProps> = ({ title, subTitle, image, layout, _type, 
                         {subTitle}
                     </Typography>
                 </div>
-                <div className="w-[50%] flex justify-center">
-                    <img src={sanityImgUrl(image).width(900).url()} alt="" className="rounded-3xl border-black border-[3px]" />
+                <div className="md:w-[50%] flex justify-center">
+                    <img src={sanityImgUrl(image).width(900).url()} alt="" className="my-4 md:my-0 border-y-[3px] md:rounded-3xl md:border-black md:border-[3px]" />
                 </div>
             </div>
         </div>

0f239f31bd32e22c4f84bd4a8d41d5d314b757d8
Antoine Ridard <papatou15@hotmail.com>
2025-04-02 21:25:01 -0400

    added animated burger menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-"use client"
+"use client";
 
+import { useState } from "react";
 import MFButton from "./MFButton";
+import { motion } from "framer-motion";
 import { PageMaker, Contact } from "@/sanity.types";
 import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import MFLink from "./MFLink";
 import sanityImgUrl from "../sanityImageBuilder";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 interface Props {
     tabs: PageMaker[];
@@ -13,33 +16,39 @@ interface Props {
 }
 
 const Header: React.FC<Props> = ({ tabs, logo }) => {
-
-    const { isSignedIn } = useUser()
+    const { isSignedIn } = useUser();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <div className="header-wrapper w-full flex md:sticky top-0 z-50 items-center justify-center">
-            <div className="header m-4 w-full h-full bg-gradient-to-l from-primary-blue to-[#0D5E68] flex flex-row rounded-2xl shadow-[0_55px_50px_-25px_rgba(0,0,0,0.25)]">
+        <div className="header-wrapper lg:w-full sticky top-4 z-50 items-center justify-center">
+            <div className="header my-4 lg:m-4 w-full h-full bg-gradient-to-l from-primary-blue to-[#0D5E68] flex flex-row items-center rounded-2xl shadow-[0_55px_50px_-25px_rgba(0,0,0,0.25)]">
                 <div className="pl-6 py-2 min-w-20">
-                    <img src={sanityImgUrl(logo?.headerLogo).height(75).fit("clip").url()} alt="Maison de la Famille de St-François" />
+                    <img 
+                        src={sanityImgUrl(logo?.headerLogo).height(75).fit("clip").url()} 
+                        alt="Maison de la Famille de St-François" 
+                    />
                 </div>
-                <div className="flex flex-row flex-wrap w-fit justify-center">
-                    {tabs ? tabs.map((tab) => {
-                        return (
-                            <MFLink link={`/${tab.slug?.current == "accueil" ? "" : tab.slug?.current}`} style="smallcolorless" key={tab._id} extraCSS="h-auto mx-10 md:mx-12 text-off-white text-xl shadow-text-sm" _type={"button"}>
-                                {tab.title || "No Title"}
-                            </MFLink>
-                        );
-                    }) : "no tabs"}
+
+                <div className="hidden lg:flex flex-row flex-wrap w-fit justify-center">
+                    {tabs?.map((tab) => (
+                        <MFLink 
+                            link={`/${tab.slug?.current == "accueil" ? "" : tab.slug?.current}`} 
+                            style="smallcolorless" 
+                            key={tab._id} 
+                            extraCSS="h-auto mx-10 md:mx-12 text-off-white text-xl shadow-text-sm" 
+                            _type="button"
+                        >
+                            {tab.title || "No Title"}
+                        </MFLink>
+                    ))}
                 </div>
-                <div className="flex items-center flex-row ml-auto">
-                    {
-                        !isSignedIn ?
-                            <SignInButton mode="modal">
-                                <MFButton style="smallbg" _type="button">
-                                    Connexion
-                                </MFButton>
-                            </SignInButton> 
-                        :
+
+                <div className="hidden lg:flex items-center flex-row ml-auto">
+                    {!isSignedIn ? (
+                        <SignInButton mode="modal">
+                            <MFButton style="smallbg" _type="button">Connexion</MFButton>
+                        </SignInButton>
+                    ) : (
                         <>
                             <UserButton
                                 showName={true}
@@ -53,9 +62,84 @@ const Header: React.FC<Props> = ({ tabs, logo }) => {
                             />
                             <MFLink _type="button" link="/account" style="smallbg">Portail de membre</MFLink>
                         </>
-                }
+                    )}
                 </div>
+
+                {/* Mobile Menu Button */}
+                <button className="lg:hidden ml-auto mr-6" onClick={() => setMenuOpen(!menuOpen)}>
+                    <div
+                        className="relative w-8 h-8 flex items-center justify-center"
+                    >
+                        <motion.div
+                            animate={menuOpen ? { rotate: 45, y: 0 } : { rotate: 0, y: -6 }}
+                            className="absolute w-6 h-0.5 bg-white"
+                        />
+                        <motion.div
+                            animate={menuOpen ? { opacity: 0 } : { opacity: 1 }}
+                            className="absolute w-6 h-0.5 my-1 bg-white"
+                        />
+                        <motion.div
+                            animate={menuOpen ? { rotate: -45, y: 0 } : { rotate: 0, y: 6 }}
+                            className="absolute w-6 h-0.5 bg-white"
+                        />
+                    </div>
+                </button>
             </div>
+
+            {/* Mobile Menu */}
+            <motion.div 
+                initial={{ height: 0 }}
+                animate={{ height: menuOpen ? "auto" : 0, opacity: menuOpen ? 1 : 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className={`overflow-hidden lg:hidden absolute left-0 w-full bg-gradient-to-l from-primary-blue to-[#0D5E68] p-4 rounded-2xl shadow-lg z-40`}
+            >
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: menuOpen ? 1 : 0 }}
+                    transition={{ delay: 0.2, duration: 0.3, ease: "easeInOut" }}
+                    className="flex flex-col items-center space-y-4"
+                >
+                    {tabs?.map((tab, index) => (
+                        <motion.div
+                            key={tab._id}
+                            initial={{ y: -20  }}
+                            animate={menuOpen ? { y: 0 } : { y: -20  }}
+                            transition={{ delay: 0.1 * index, duration: 0.5, ease: "easeOut", type: "spring", stiffness: 300 }}
+                        >
+                            <MFLink 
+                                link={`/${tab.slug?.current == "accueil" ? "" : tab.slug?.current}`} 
+                                style="smallcolorless" 
+                                extraCSS="text-off-white text-lg" 
+                                _type="button"
+                                onClick={() => setMenuOpen(false)}
+                            >
+                                {tab.title || "No Title"}
+                            </MFLink>
+                        </motion.div>
+                    ))}
+                    <div className="flex flex-col items-center space-y-4">
+                        {!isSignedIn ? (
+                            <SignInButton mode="modal">
+                                <MFButton style="smallbg" _type="button">Connexion</MFButton>
+                            </SignInButton>
+                        ) : (
+                            <>
+                                <UserButton
+                                    showName={true}
+                                    appearance={{
+                                        elements: {
+                                            userButtonBox: 'h-6 bg-off-white hover:bg-primary-blue p-4 rounded-2xl text-black border-2 border-black',
+                                            userButtonPopoverMain: 'border-2 border-black rounded-2xl text-black',
+                                            userButtonPopoverActionButton: 'text-black',
+                                        }
+                                    }}
+                                />
+                                <MFLink _type="button" link="/account" style="smallbg">Portail de membre</MFLink>
+                            </>
+                        )}
+                    </div>
+                </motion.div>
+            </motion.div>
         </div>
     );
 };
